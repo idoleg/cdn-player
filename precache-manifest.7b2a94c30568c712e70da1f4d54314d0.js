232a8493ddb202d8611b0f33028b098c4f8617ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-03 12:57:08 +0000

    Deploy idoleg/cdn-player to github.com/idoleg/cdn-player.git:gh-pages

---
+++ precache-manifest.7b2a94c30568c712e70da1f4d54314d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61c7f855b6c59f125b2d858fdcf6076",
+    "url": "/cdn-player/index.html"
+  },
+  {
+    "revision": "482425b6abfbca39582e",
+    "url": "/cdn-player/static/css/2.8ebbe4b8.chunk.css"
+  },
+  {
+    "revision": "2d5c5733f983ca8b2222",
+    "url": "/cdn-player/static/css/main.d8199224.chunk.css"
+  },
+  {
+    "revision": "482425b6abfbca39582e",
+    "url": "/cdn-player/static/js/2.5de2627b.chunk.js"
+  },
+  {
+    "revision": "2d5c5733f983ca8b2222",
+    "url": "/cdn-player/static/js/main.bdbdd9eb.chunk.js"
+  },
+  {
+    "revision": "760ac25dbcca491eea90",
+    "url": "/cdn-player/static/js/runtime~main.2d809ab2.js"
+  }
+]);
